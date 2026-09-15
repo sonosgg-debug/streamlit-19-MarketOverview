0311657a3233297049992d96fccfee302d67b9d9
sonosgg-debug <sonosgg@gmail.com>
2026-09-15 23:53:37 +0900

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Market Overview Dashboard (Streamlit)
-cd /d "%~dp0"
-echo ===================================================
-echo   Market Overview Dashboard (Streamlit)
-echo ===================================================
-echo Launching application in your default browser...
-python -m streamlit run app.py
-pause
